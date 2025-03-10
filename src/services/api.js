10fa0f8991a49493b998d96401886fe542885229
kdevrama <rama.gusti@karteldaun.com>
2025-03-10 12:32:18 +0700

    Fix: change to new domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://54.169.199.30/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://ragst.vip/api",
   headers: {
     "Content-Type": "application/json",
   },
